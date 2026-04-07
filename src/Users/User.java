e48cc3dddd3b2486d42a50569788c96d15f47811
Victor <v.johnokoh@gmail.com>
2026-04-08 00:26:24 +0100

    Made viewproducts abstract so it could be called from a Product variable without having to cast the variable to one of Product's subclasses Changed descOrder to accept an ArrayList<Product> and return an ordered ArrayList<Product>

--- src/Users/User.java
+++ src/Users/User.java
@@ -1,6 +1,9 @@
 package Users;
 
+import Inventory.Product;
+
 import java.io.File;
+import java.io.IOException;
 import java.nio.file.Files;
 import java.util.ArrayList;
 import java.util.List;
@@ -10,6 +13,7 @@ public abstract class User {
    private String name;
    private Address address;
    private final String role;
+   private final static File stockFile = new File("Stock.txt");
 
    public User(int id, String n, Address addr, String r){
        userID = id;
@@ -17,6 +21,11 @@ public abstract class User {
        address = addr;
        role = r;
    }
+
+   public File getStockFile(){
+       return stockFile;
+   }
+
    public String getRole(){
        return role;
    }
@@ -33,36 +42,18 @@ public abstract class User {
        return address;
    }
 
-   public StringBuilder viewProducts(File stockFile) throws Exception{
-       List<String> lines = Files.readAllLines(stockFile.toPath());
-       ArrayList<List<String>> splitLines = new ArrayList<>();
-       for (String line : lines) {
-           splitLines.add(List.of(line.split(";")));
-       }
-       String[][] orderedLines =  descOrder(splitLines);
-       StringBuilder output = new StringBuilder();
-       for (String[] orderedLine : orderedLines) {
-           for (int j = 0; j < orderedLines[0].length; j++) {
-               output.append(orderedLine[j]);
-               if (j < orderedLine.length-2){
-                   output.append(",");
-               }
-           }
-           output.append("\n");
-       }
-       return output;
-   }
+   public abstract String viewProducts() throws IOException;
     /*
-        Takes in a 2D array of the stock data from Stock.txt and uses a Bubble sort
-        to order it in descending order then returns a StringBuilder to be printed
-        */
-    public String[][] descOrder(ArrayList<List<String>> list){
-        List<String> temp;
+     Takes in an array of Products and sorts them into descending order of price
+     then returns an array of Products
+    */
+    public ArrayList<Product> descOrder(ArrayList<Product> list){
+        Product temp;
         for (int i = 0; i<list.size()-2; i++){
             boolean swapped = false;
             for (int j = 0; j<list.size()-2-i; j++) {
-                double currentPrice = Double.parseDouble(list.get(j).get(4).trim());
-                double nextPrice = Double.parseDouble(list.get(j+1).get(4).trim());
+                double currentPrice = list.get(j).getPrice();
+                double nextPrice = list.get(j+1).getPrice();
                 if (nextPrice > currentPrice){
                     temp = list.get(j);
                     list.set(j, list.get(j+1));
@@ -74,24 +65,7 @@ public abstract class User {
                 break;
             }
         }
-        // The -1 of the list size removes the empty list at the end of the returned arraylist
-        String[][] final_arr = new String[list.size()-1][list.getFirst().size()];
-        for (int i = 0; i< final_arr.length; i++){
-            for (int j = 0; j<final_arr[1].length; j++){
-                final_arr[i][j] = list.get(i).get(j);
-            }
-        }
-//        StringBuilder orderedList = new StringBuilder();
-//        for (List<String> strings : list) {
-//            for (int j = 0; j < strings.size(); j++) {
-//                orderedList.append(strings.get(j));
-//                if (j < strings.size() - 1) {
-//                    orderedList.append(", ");
-//                }
-//            }
-//            orderedList.append("\n");
-//        }
-//        return orderedList;
-        return final_arr;
+        return list;
     }
+
 }
