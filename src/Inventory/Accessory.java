98e183016823da307d8ea3a8f286da7911e7ec32
Victor <v.johnokoh@gmail.com>
2026-04-08 00:31:45 +0100

    Added partString method to hide purchaseCost from the Customer

--- src/Inventory/Accessory.java
+++ src/Inventory/Accessory.java
@@ -1,8 +1,8 @@
-package Inventory;
+package Inventory ;
 
 public class Accessory extends Product{
     final private String compatibility;
-    final String type;
+    final private String type;
 
     public Accessory(String[] line){
         super(Integer.parseInt(line[0].trim()), ProductCategory.ACCESSORY, line[3].trim(), Double.parseDouble(line[6].trim()), Integer.parseInt(line[5].trim()), Double.parseDouble(line[4].trim()));
@@ -15,5 +15,10 @@ public class Accessory extends Product{
     @Override
     public String toString() {
         return String.format("%d; %s; %s; %s; %.2f; %d; %.2f; %s", getProductID(), "accessory", type, getProductName(), getPrice(), getQuantityInStock(), getPurchaseCost(), getCompatibility());
+    }
+
+    public String partString(){
+        return String.format("%d; %s; %s; %s; %.2f; %d; %s", getProductID(), "accessory", type, getProductName(), getPrice(), getQuantityInStock(), getCompatibility());
+
     }
 }
