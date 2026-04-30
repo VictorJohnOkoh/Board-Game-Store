92f22567cd1b7c704e2f5bf2ab85b531594fb41a
Victor <v.johnokoh@gmail.com>
2026-04-30 18:16:19 +0100

    Added exception handling for customer and admin CLI input

--- src/CLIbasis/CLIbasis/AdminCLI.java
+++ src/CLIbasis/CLIbasis/AdminCLI.java
@@ -16,44 +16,47 @@ public class AdminCLI {
         while (true) {
         	printAdminMenu();
 
-        	int selection = Integer.parseInt(consoleInput.nextLine().trim());
-        	
-        	switch (selection) {
-        		case 1:
-					System.out.println("ProdID; Prod Category; Type; Name; Price; Stock; Purchase Cost; Extra");
-        			System.out.println(admin.viewProducts());
-        			System.out.println();
-        			break;
-        		
-        		case 2:
-					System.out.println();
-					System.out.println("1) Add a board game");
-					System.out.println("2) Add an accessory");
-					System.out.println("PRESS ANY OTHER NUMBER BUTTON TO CANCEL");
-					int choice = Integer.parseInt(consoleInput.nextLine());
-					switch (choice) {
-						case 1:
-							System.out.println();
-							admin.addBoardGame(consoleInput);
-							break;
-						case 2:
-							System.out.println();
-							admin.addAccessory(consoleInput);
-							break;
-						default:
-							break;
+            try {
+                int selection = Integer.parseInt(consoleInput.nextLine().trim());
 
-					}
-        			System.out.println();
-        			break;
-        			
-        		case 0:
-        			return;
-        			
-        		default:
-        			System.out.println(INVALID);
-        			System.out.println();
-        	}
+                switch (selection) {
+                    case 1:
+                        System.out.println(admin.viewProducts());
+                        System.out.println();
+                        break;
+
+                    case 2:
+                        System.out.println();
+                        System.out.println("1) Add a board game");
+                        System.out.println("2) Add an accessory");
+                        System.out.println("PRESS ANY OTHER NUMBER BUTTON TO CANCEL");
+                        int choice = Integer.parseInt(consoleInput.nextLine());
+                        switch (choice) {
+                            case 1:
+                                System.out.println();
+                                admin.addBoardGame(consoleInput);
+                                break;
+                            case 2:
+                                System.out.println();
+                                admin.addAccessory(consoleInput);
+                                break;
+                            default:
+                                break;
+
+                        }
+                        System.out.println();
+                        break;
+
+                    case 0:
+                        return;
+
+                    default:
+                        System.out.println(INVALID);
+                        System.out.println();
+                }
+            } catch (NumberFormatException e) {
+                System.out.println("Entry type is not recognised.\n");
+            }
         }
     }
     
